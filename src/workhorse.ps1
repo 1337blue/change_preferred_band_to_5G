df2297d548674926d910f71149ca0a1975d8cfcf
Phillip L <46860631+1337blue@users.noreply.github.com>
2020-02-20 11:42:30 +0100

    Changed RegistryValue to 2

--- src/workhorse.ps1
+++ src/workhorse.ps1
@@ -7,6 +7,6 @@ if ((Get-PrefferedBand).length -ge 0) {
     echo "Something went wrong here - Please contact your administrator"
 } 
 else {
-    Set-NetAdapterAdvancedProperty -RegistryKeyword "RoamingPreferredBandType" -RegistryValue "0"
+    Set-NetAdapterAdvancedProperty -RegistryKeyword "RoamingPreferredBandType" -RegistryValue "2"
     echo "Everything done - exiting"
 }
